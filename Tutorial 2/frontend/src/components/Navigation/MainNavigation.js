946de5b78d7f243889232d27cc302129985a76e7
sinawecuba <sinawesome@gmail.com>
2026-02-27 14:54:42 +0200

    MainNavigation.js

--- Tutorial 2/frontend/src/components/Navigation/MainNavigation.js
+++ Tutorial 2/frontend/src/components/Navigation/MainNavigation.js
@@ -10,7 +10,7 @@ const mainNavigation = props => (
       return (
         <header className="main-navigation">
           <div className="main-navigation__logo">
-            <h1>EasyEvent</h1>
+            <h1>Booking Good</h1>
           </div>
           <nav className="main-navigation__items">
             <ul>
